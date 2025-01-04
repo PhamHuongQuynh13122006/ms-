d140ed9b99552d9cb580375fcedf50f8e608ddf0
PhamHuongQuynh13122006 <quynh2682@icloud.com>
2025-01-04 19:25:37 +0700

    Update function.h

--- function.h
+++ function.h
@@ -1,5 +1,3 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
 #include "datatype.h"
 //Nguyen mau ham
 void printSlowly(const char *mes, int delay);
@@ -15,4 +13,4 @@ void deleted(struct Student students[], int *n);
 void sort_Student_i_or_d(struct Student students[100],int n);
 //void save_file(struct Student *students, int n);
 //int load_file(struct Student *students, int *n);
-#endif 
+
