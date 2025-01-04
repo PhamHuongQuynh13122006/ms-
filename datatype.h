a7dd3a86c256c128a9a3f2dc78d21ae742ee7395
PhamHuongQuynh13122006 <quynh2682@icloud.com>
2025-01-05 01:14:42 +0700

    Update datatype.h

--- datatype.h
+++ datatype.h
@@ -14,20 +14,3 @@ struct Student{
    // char password[50];
     int number_course;
 };
-struct Classroom {
-    char classroomId[10];
-	char teacherId[10];    
-	char teacherName[20];    
-	char classroomName[10];
-	};
-struct Teacher{
-	char teacher[10];
-	char classroom[10];
-	char Name[20];
-	struct Date;
-	bool gender;
-	char email[30];
-	char phone[20];
-	char password[20];
-	
-};
